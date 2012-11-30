0a3e4d6b460aa3e2ca4360f6980dee401e7e059e
Nathan Snodgrass <nathaniel.snodgrass@students.borgia.com>
2012-11-29 21:50:18 -0600

    Added conditional statement to the localtime.

--- perl-hello-world.pl
+++ perl-hello-world.pl
@@ -1,3 +1,9 @@
 #!/usr/bin/perl
 print "Hello, World\n";
-dfdfdprint join ",",  localtime;
+ join ",",  localtime;
+if ( ((localtime())[0]) % 2 == 0 ) {   # If the seconds in the current minute are even
+   print scalar localtime, "\n";
+   } else {
+      print "Hello, World!\n";
+      }
+              
